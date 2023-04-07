a498aa91d829f8fd02a107865c11361196e5dfbd
-V- <123872869+Armageddon47@users.noreply.github.com>
2023-04-07 16:20:10 +0330

    end of part 1

---
+++ main.py
@@ -0,0 +1,48 @@
+files = []
+totalSize = 0
+
+with open('input.txt','r') as f:
+    for l in f:
+        
+        files.append(l.strip())
+
+path = "/main"
+dirs = {"/main":0}
+
+for command in files:
+    
+    if command[0] == "$":
+        if command [2:4] == "ls":
+            pass
+        elif command[2:4] == "cd":
+        
+            if command [5:6] == "/": ##if its cd / means its base dir
+                path = "/main"
+                    
+            elif command [5:7] == "..":
+                path = path[:path.rfind("/")]
+            
+            else:
+                dir_name = command[5:]
+                path = path + "/" + dir_name
+                dirs.update({path:0})
+                
+    elif command[0:3] == "dir":
+        pass
+    
+    else:
+        size = int(command[:command.find(" ")])
+        
+        dir = path
+        
+        for i in range(path.count("/")):
+            dirs[dir] += size
+            dir = dir[:dir.rfind("/")]
+            
+for i in dirs:
+    if dirs[i] <100001:
+        totalSize += dirs[i]
+        
+print (totalSize)
+
+# end of part 1
